76faab9a0c2bc953c3d504aa80949c8839d3fc85
Morgan Herlocker <morgan.herlocker@gmail.com>
2012-10-05 14:32:43 -0400

    setup home ui

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+/*
+ * GET mobile page
+ */
+
+exports.mobile = function(req, res){
+  res.render('mobile', { title: 'Express' });
+};
